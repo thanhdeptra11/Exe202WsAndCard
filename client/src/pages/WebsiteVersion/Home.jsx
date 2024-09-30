5677ded6affb1a956d9ae54543ebb8e759154d03
dnthchung <doanthanhchung03nb@gmail.com>
2024-10-01 03:43:38 +0700

    remove

--- client/src/pages/WebsiteVersion/Home.jsx
+++ client/src/pages/WebsiteVersion/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Hero from "../../components/Hero.jsx";
+import Hero from "../../components/Hero";
 import HowItWorks from "../../components/HowItWorks";
 import Services from "../../components/Services";
 import Testimonials from "../../components/Testimonials";
