d3b383889e8316e4e1b2d9deee8542fb37dc6c41
dnthchung <doanthanhchung03nb@gmail.com>
2024-10-12 20:21:45 +0700

    mdf

--- src/components/admin/AdminSidebar.jsx
+++ src/components/admin/AdminSidebar.jsx
@@ -1,31 +1,26 @@
 import { Link } from "react-router-dom";
 import { IconHome, IconUsers, IconChartBar, IconSettings } from "@tabler/icons-react";
 
+const sidebarItems = [
+  { name: "Home", icon: IconHome, link: "/" },
+  { name: "Users", icon: IconUsers, link: "/admin/users" },
+  { name: "Shop", icon: IconSettings, link: "/admin/shop" },
+  { name: "Analytics", icon: IconChartBar, link: "/admin/analytics" },
+];
+
 function AdminSidebar({ isOpen }) {
   return (
     <aside className={`bg-gray-800 text-white w-64 min-h-screen p-4 ${isOpen ? "" : "hidden"}`}>
       <nav>
         <ul className="space-y-2">
-          <li>
-            <Link to="/" className="flex items-center p-2 hover:bg-gray-700 rounded">
-              <IconHome className="mr-2" /> Home
-            </Link>
-          </li>
-          <li>
-            <Link to="/users" className="flex items-center p-2 hover:bg-gray-700 rounded">
-              <IconUsers className="mr-2" /> Users
-            </Link>
-          </li>
-          <li>
-            <Link to="/analytics" className="flex items-center p-2 hover:bg-gray-700 rounded">
-              <IconChartBar className="mr-2" /> Analytics
-            </Link>
-          </li>
-          <li>
-            <Link to="/settings" className="flex items-center p-2 hover:bg-gray-700 rounded">
-              <IconSettings className="mr-2" /> Settings
-            </Link>
-          </li>
+          {sidebarItems.map((item, index) => (
+            <li key={index}>
+              <Link to={item.link} className="flex items-center p-2 hover:bg-gray-700 rounded">
+                <item.icon className="mr-2" />
+                {item.name}
+              </Link>
+            </li>
+          ))}
         </ul>
       </nav>
     </aside>
